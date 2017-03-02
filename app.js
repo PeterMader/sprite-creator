12ccd852730c2f4b179e590a712b56e49377593d
PeterMader <peter_mader@gmx.de>
2017-03-02 16:48:28 +0100

    Add space between images.

--- app.js
+++ app.js
@@ -4,31 +4,65 @@ document.addEventListener('DOMContentLoaded', () => {
   const dropZone = document.getElementById('drop-zone')
   const ctx = canvas.getContext('2d')
   const button = document.getElementById('submit')
+  const details = document.getElementById('details')
+  const space = document.getElementById('space')
+
+  canvas.height = 0
+  canvas.width = 0
 
   const sprite = {
     items: 0,
-    width: 0,
-    height: 0,
     images: []
+  }
+
+  const remove = function () {
+    while (details.firstChild) {
+      details.removeChild(details.firstChild)
+    }
+  }
+
+  // parse the input value and round it
+  const getSpace = () => (Number(space.value) + 0.5) | 0
+
+  const update = function () {
+    const {images} = sprite
+    let index
+    let y = 0
+
+    remove()
+
+    for (index in images) {
+      const item = images[index]
+      ctx.drawImage(item, 0, y)
+
+      const element = document.createElement('div')
+      element.classList.add('details-item')
+      element.style.top = y + 'px'
+      element.textContent = item.fileName + '; y=' + y
+      details.appendChild(element)
+
+      y += item.height + getSpace()
+    }
+  }
+
+  const setHeight = function () {
+    canvas.height = sprite.images.reduce((accumulator, image) => {
+      return accumulator + image.height
+    }, 0) + getSpace() * (sprite.items - 1)
   }
 
   const isImageRegex = /image.*/
 
-  const addImage = (image) => {
+  const addImage = function (image) {
     const {images} = sprite
     images.push(image)
     image.onload = function () {
-      if (image.width > sprite.width) {
-        canvas.width = sprite.width = image.width
+      if (image.width > canvas.width) {
+        canvas.width = image.width
+        details.style.left = (canvas.width + 20) + 'px'
       }
-      canvas.height = sprite.height += image.height
-      let index
-      let y = 0
-      for (index in images) {
-        const item = images[index]
-        ctx.drawImage(item, 0, y)
-        y += item.height
-      }
+      setHeight()
+      update()
     }
   }
 
@@ -42,10 +76,12 @@ document.addEventListener('DOMContentLoaded', () => {
       const file = files[index]
       if (isImageRegex.test(file.type)) {
         const reader = new FileReader()
+        sprite.items += 1
 
         reader.onload = function (imageEvent) {
           var image = new Image()
           image.src= imageEvent.target.result
+          image.fileName = file.name
           addImage(image)
         }
 
@@ -70,6 +106,20 @@ document.addEventListener('DOMContentLoaded', () => {
     } else {
       ev.dataTransfer.clearData()
     }
+  })
+
+  document.getElementById('clear').addEventListener('click', () => {
+    sprite.items = 0
+    sprite.images = []
+    canvas.width = 0
+    canvas.height = 0
+
+    remove()
+  })
+
+  space.addEventListener('change', function () {
+    setHeight()
+    update()
   })
 
   button.addEventListener('click', (e) => {
